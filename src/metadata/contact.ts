f07ac24c2b3b649f6e51b70f400b3786e2830ec4
Prabhjot Singh <prabhjotsingh-dev@outlook.com>
2026-06-12 19:52:17 +0530

    Feature/20 add dark mode (#17)
    feature/20 add dark mode

---
+++ src/metadata/contact.ts
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Us",
+  description: "Get in touch with Punjabi Typing. Send us your feedback, questions, or suggestions.",
+  openGraph: {
+    title: "Contact Us | Punjabi Typing",
+    description: "Get in touch with Punjabi Typing. Send us your feedback, questions, or suggestions.",
+    url: "https://typingpunjabi.vercel.app/contact",
+  }
+};
